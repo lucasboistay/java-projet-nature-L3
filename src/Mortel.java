c8d3fd8d2c650fd72f0002a85c46ef3c1bf80108
Lucas <lucasboistay@hotmail.fr>
2023-01-27 17:07:11 +0100

    modif main

--- src/Mortel.java
+++
@@ -1,7 +0,0 @@
-public interface Mortel {
-    public int getEnergie();
-    public int getDureeVie();
-    public void mange();
-    public void energieDiminue();
-    public void tempsPasseVie();
-}
--- src/TouteCasePleine.java
+++
@@ -1,7 +0,0 @@
-public class TouteCasePleine extends Exception {
-    private static final long serialVersionUID = 1L;
-
-    public TouteCasePleine(){
-        super("Toutes les cases du Jardin sont remplis, pas de mouvement possible");
-    }
-}
